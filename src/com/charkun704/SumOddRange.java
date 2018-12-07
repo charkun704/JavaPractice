a1b1bb7e0514829da2baaf333e4ae1066543481b
charkun704 <cvldz03@gmail.com>
2018-12-07 17:08:44 -0500

    Update gitignore and complete SumOddRange

--- src/com/charkun704/SumOddRange.java
+++ src/com/charkun704/SumOddRange.java
@@ -2,21 +2,23 @@ package com.charkun704;
 
 public class SumOddRange {
 
-    public static void main(String[] args) {
-        System.out.println(sumOdd(1,100));
-    }
+//    public static void main(String[] args) {
+//        System.out.println(sumOdd(100,-100));
+//    }
 
     public static boolean isOdd(int number){
         int remainder = number%2;
-        if(remainder==0)return true;
+        if(remainder==1)return true;
         else return false;
     }
     public static int sumOdd(int start, int end){
         if(end<start || start < 0) return -1;
         else{
-            int sum;
+            int sum = 0;
             for (int i = start; i <= end; i++){
-                if(isOdd(i)) sum += i;
+                if(isOdd(i)){
+                    sum = sum + i;
+                }
             }
             return sum;
         }
