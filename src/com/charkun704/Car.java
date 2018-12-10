6f36d48b813bbc522fc869fd0100a9dfe3c77eba
charkun704 <cvldz03@gmail.com>
2018-12-10 14:16:11 -0500

    Learning inheritance using Animal and Dog.

---
+++ src/com/charkun704/Car.java
@@ -0,0 +1,12 @@
+package com.charkun704;
+
+public class Car {
+    private int doors;
+    private int wheels;
+    public String model;
+    private String engine;
+    private String colour;
+
+    public void setModel(String model){ this.model = model; }
+    public String getModel(){ return this.model; }
+}
---
+++ src/com/charkun704/Main.java
@@ -0,0 +1,28 @@
+package com.charkun704;
+/*
+This class will be used to hold all the Main class functions for when it's needed.
+ */
+public class Main {
+    /*
+    121018
+    Practicing inheritance.
+     */
+    public static void main(String[] args) {
+        Animal animal = new Animal("Animal",1,1,5,5);
+        Dog dog= new Dog("Yorkie",8,20,2,4,1,20,"Long silky");
+        dog.eat();
+    }
+
+
+    /*
+    Main class for Car class exercise. 120818
+     */
+//    public static void main(String[] args) {
+//
+//        Car honda = new Car();
+//
+//        honda.setModel("civic");
+//
+//        System.out.println(honda.getModel());
+//    }
+} // END OF MAIN
